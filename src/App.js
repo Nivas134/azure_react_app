d8d879cf5e4c4aedb2130fd4a161ed6fb2dcab9f
Nivas134 <kalyankumar767399@gmail.com>
2025-04-05 18:04:52 +0530

    updated changes to test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">React Azure App </header>
+      <header className="App-header">React Azure App- Updated changes.</header>
     </div>
   );
 }
